6e4bcb143a21033b0636212c84708718a7fbb29f
Mohammad <m@fr33.tv>
2026-01-21 17:13:10 +0530

    App uninstalled clears the llm generated flag

--- app/Jobs/AppUninstalledJob.php
+++ app/Jobs/AppUninstalledJob.php
@@ -2,6 +2,31 @@
 
 namespace App\Jobs;
 
+use Osiset\ShopifyApp\Contracts\Commands\Shop;
+use Osiset\ShopifyApp\Contracts\Queries\Shop as QueriesShop;
+use Osiset\ShopifyApp\Actions\CancelCurrentPlan;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+
 class AppUninstalledJob extends \Osiset\ShopifyApp\Messaging\Jobs\AppUninstalledJob
 {
+    public function handle(Shop $shopCommand, QueriesShop $shopQuery, CancelCurrentPlan $cancelCurrentPlanAction): bool
+    {
+        parent::handle($shopCommand, $shopQuery, $cancelCurrentPlanAction);
+
+        // clear the llm generated at field
+        $shop = User::withTrashed()->where('name', $this->domain)->first();
+        if ($shop) {
+            $shop->llm_generated_at = null;
+            $shop->save();
+        }
+
+        // clear the llm.txt file from storage
+        $filename = "llm/{$shop->id}.txt";
+        if (Storage::exists($filename)) {
+            Storage::delete($filename);
+        }
+
+        return true;
+    }
 }
